73b48e16a769004c8ed6369f763227d7a32f85e0
Mcelest19 <mrn.celestino@gmail.com>
2023-04-06 09:51:14 +0200

    delete auth for movies

--- index.js
+++ index.js
@@ -48,15 +48,17 @@ app.get('/', (req, res) => {
 });
 
 // return JSON object with all movies  (Return a list of ALL movies to the user)
-app.get("/movies", passport.authenticate('jwt', { session: false }), (req, res)=> {
-  Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err)=> {
-      console.error(err);      
-      res.status(500).send("Error: "+ err);
-    });
+app.get("/movies", 
+ // passport.authenticate('jwt', { session: false }), 
+  (req, res)=> {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err)=> {
+        console.error(err);      
+        res.status(500).send("Error: "+ err);
+      });
 });
 
 //get data about single movie name  (Return data about a single movie by title to the user)
